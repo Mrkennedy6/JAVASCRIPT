d20d965f60e3a5e4d301e8ac36c0ae15aed59f1c
Mrkennedy6 <jesusferruzca666@gmail.com>
2025-02-04 21:20:14 -0600

    first commit

---
+++ tipos-de-datos.js
@@ -0,0 +1,18 @@
+console.log("hola mundo");
+
+//declaracion de variables;
+var nombre = "Ana";
+var edad = 23;
+var sueldo = 1.234;
+var casada = false;
+var vacunas = true;
+var hijos = null;
+var dato; // undefined 
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casada));
+console.log(typeof(vacunas));
+console.log(typeof(hijos));
+console.log(typeof(dato));
